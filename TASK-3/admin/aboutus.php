adbf443a684c4752ed5391dcf8dcf072559e611c
Sairam-52 <93109567+Sairam-52@users.noreply.github.com>
2021-11-20 17:18:32 +0530

    Update aboutus.php

--- TASK-3/admin/aboutus.php
+++ TASK-3/admin/aboutus.php
@@ -19,9 +19,9 @@
         </div>
       </nav>
       <div class="main-content-header">
-          <h2 class="search">About Us</h3>
+          <h3 class="search">About Us</h3>
              <div class="div2">
-             <h3>Student Result Management System</h2>
+             <h3>Student Result Management System</h3>
              <p>This Software Application unbelievably unravels and quickens the result management system with unique templates by providing the administration a secure database system for storing, evaluating and publishing the test scores and grades of candidates online. The database likewise allows the student to observe and gander at the exam results on the web at whatever point necessary.</p>
              <p> Student Result Management System is a technological opportunity for the school, college university and coaching centre institutions searching for a secure, simple and alternative solution to the conventional paper-based exam results  evaluation, reporting and distribution.</p>
              </div>  
